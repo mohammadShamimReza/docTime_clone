efe42782f4dffd520e50f7916c9bc4b1afd13ab5
GitMohammad <mohammadshamimreza23393@gmail.com>
2022-05-17 16:46:52 +0600

    start createing navbar

---
+++ src/pages/navigation/Navigation.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Navigation = () => {
+    return (
+        <div>
+            <div className="navigation-logo">
+
+            </div>
+            <div className="navigation-button">
+                <a href="/#">How DocTime Works</a>
+                <a href="/#">ForDoctors</a>
+                <a href="/#">DocTime for Enterprises</a>
+                <a href="/#">About Us</a>
+                <a href="/#">FAQs</a>
+            </div>
+        </div>
+    );
+};
+
+export default Navigation;
