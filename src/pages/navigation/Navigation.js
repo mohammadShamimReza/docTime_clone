8ec581dd7f8e1d3c75d963e1c77748f6df14a36e
GitMohammad <mohammadshamimreza23393@gmail.com>
2022-05-17 21:57:58 +0600

    simple style manus

--- src/pages/navigation/Navigation.js
+++ src/pages/navigation/Navigation.js
@@ -1,20 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
+
 
 const Navigation = () => {
     return (
-        <div>
-            <div className="navigation-logo">
-
-            </div>
-            <div className="navigation-button">
+        <Container>
+            <Wrap>
+                <img src="" alt="" />
+            </Wrap>
+            <Manu>
                 <a href="/#">How DocTime Works</a>
                 <a href="/#">ForDoctors</a>
                 <a href="/#">DocTime for Enterprises</a>
                 <a href="/#">About Us</a>
                 <a href="/#">FAQs</a>
-            </div>
-        </div>
+            </Manu>
+        </Container>
     );
 };
+
+const Manu = styled.div`
+    text-align: right;
+    a {
+        text-decoration: none;
+    }
+`
+const Container = styled.div`
+
+`;
+const Wrap = styled.div`
+    
+`;
+
 
 export default Navigation;
