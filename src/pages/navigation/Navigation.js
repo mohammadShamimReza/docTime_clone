ef4b6412bc7aa7fe9bc809ef2c55de0f97b2caf6
GitMohammad <mohammadshamimreza23393@gmail.com>
2022-05-19 15:27:47 +0600

    create nav manu

--- src/pages/navigation/Navigation.js
+++ src/pages/navigation/Navigation.js
@@ -5,32 +5,49 @@ import styled from 'styled-components';
 const Navigation = () => {
     return (
         <Container>
-            <Wrap>
-                <img src="" alt="" />
-            </Wrap>
+            
+            <a href="#default" class="logo"><img src="./images/logo.png" alt="" /></a>
+            
             <Manu>
                 <a href="/#">How DocTime Works</a>
                 <a href="/#">ForDoctors</a>
                 <a href="/#">DocTime for Enterprises</a>
                 <a href="/#">About Us</a>
                 <a href="/#">FAQs</a>
+                <a href="/#">LogIN</a>
             </Manu>
         </Container>
     );
 };
 
-const Manu = styled.div`
-    text-align: right;
+
+const Container = styled.div`  
+    height: 115px;
+    background-color: yellow;
     a {
+        img {
+            overflow: hidden;
+            width: 300px;
+            background-color: blue;
+        }
+        float: left;
+        color: black;
+        padding: 12px;
+        border-radius: 12px;
+        text-align: center;
         text-decoration: none;
     }
-`
-const Container = styled.div`
+    a:hover {
+        background-color: #ddd;
+    }
+`;
 
-`;
-const Wrap = styled.div`
-    
-`;
+const Manu = styled.div`
+    a {
+        float: right;
+    }
+`
+
 
 
 export default Navigation;
